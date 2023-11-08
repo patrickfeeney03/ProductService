803933d3912f12f67d394ddbb1e2eecfb1e68632
Patrick Feeney <g00409592@atu.ie>
2023-11-08 14:22:32 +0000

    Add method to interface

--- src/main/java/ie/atu/productservice/ProductService.java
+++ src/main/java/ie/atu/productservice/ProductService.java
@@ -1,4 +1,12 @@
 package ie.atu.productservice;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@FeignClient(name = "warehouse-service", url = "http://localhost:8081")
 public interface ProductService {
+    @PostMapping("/add-product")
+    Object addProduct(@RequestBody Product product);
 }

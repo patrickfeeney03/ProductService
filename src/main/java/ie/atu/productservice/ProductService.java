cc711b3654e145b199cb2db4c1ce386808d6d285
Patrick Feeney <g00409592@atu.ie>
2023-11-08 14:23:37 +0000

    Create getProductById Method

--- src/main/java/ie/atu/productservice/ProductService.java
+++ src/main/java/ie/atu/productservice/ProductService.java
@@ -1,12 +1,13 @@
 package ie.atu.productservice;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "warehouse-service", url = "http://localhost:8081")
 public interface ProductService {
     @PostMapping("/add-product")
     Object addProduct(@RequestBody Product product);
+
+    @GetMapping("/get-product-by-id/{id}")
+    Object getProductById(@PathVariable String id);
 }

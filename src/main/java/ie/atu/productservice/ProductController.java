a5702c11d4fb3c109863863661b641ec87ee4f94
Patrick Feeney <g00409592@atu.ie>
2023-11-08 14:09:56 +0000

    Get MVP for add-product endpoint

--- src/main/java/ie/atu/productservice/ProductController.java
+++ src/main/java/ie/atu/productservice/ProductController.java
@@ -1,11 +1,16 @@
 package ie.atu.productservice;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
     @PostMapping("/add-product")
-    public Object addProduct(@RequestBody )
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Object addProduct(@RequestBody Product product) {
+        return "This is working";
+    }
 }

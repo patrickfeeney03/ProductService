db9c40150d01af3dfd6278a03769bfcea4ab3b4a
Patrick Feeney <g00409592@atu.ie>
2023-11-08 14:24:50 +0000

    Modify Controller

--- src/main/java/ie/atu/productservice/ProductController.java
+++ src/main/java/ie/atu/productservice/ProductController.java
@@ -1,23 +1,25 @@
 package ie.atu.productservice;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @PostMapping("/add-product")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public Object addProduct(@RequestBody Product product) {
-        return product;
+        return productService.addProduct(product);
     }
 
-
-    @PostMapping("/get-product-by-id")
+    @PostMapping("/get-product-by-id/{id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public Object getProductById(@RequestBody Warehouse warehouse) {
-        return warehouse;
+    public Object getProductById(@PathVariable String id) {
+        return productService.getProductById(id);
     }
 }
